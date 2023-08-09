a97b7501435dd4aa8287cb94438f7adb36751938
Willian Fuks <willian.fuks@gmail.com>
2023-08-09 14:57:40 -0300

    Remove old integration tests

--- integration-tests/flow/models/jaffle/staging/stg__customers.sql
+++
@@ -1,2 +0,0 @@
-select 1 as customer_id, 'nameA' as first_name, 'lastA' as last_name union all
-select 2 as customer_id, 'nameB' as first_name, 'lastB' as last_name
--- integration-tests/jaffle-shop/models/metrics_customers.sql
+++
@@ -1,6 +0,0 @@
-select * 
-from {{ metrics.calculate(
-    metric_list=[metric('average_order_amount'), metric('total_order_amount'), metric('derived_test')],
-    grain='all_time',
-    dimensions=[],
-) }}
